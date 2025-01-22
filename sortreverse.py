8825c2aa8f274c2114f1562a50f43253417a9665
Yumid03 <202480032@psu.palawan.edu.ph>
2025-01-22 15:30:27 +0800

    Create sortreverse.py

---
+++ sortreverse.py
@@ -0,0 +1,7 @@
+numbers = [5, 2, 9, 1, 5, 6]
+
+sorted_numbers = sorted(numbers)
+print("Sorted list:", sorted_numbers)
+
+reversed_numbers = list(reversed(numbers))
+print("Reversed list:", reversed_numbers)
